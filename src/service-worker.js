b7b0bbd901cf592fc92bf70df1a36b7a110849a5
Hugo Akpakoun <hugo.akpakoun@outlook.fr>
2026-02-05 13:48:12 +0100

    update : no more cache

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,20 +1,8 @@
 const CACHE_NAME = 'tekitiz-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/blog/',
-  '/services/',
-  '/about/',
-  '/offline.html'
-];
 
-// Install event
+// Install event - don't cache anything on install
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
-  );
+  self.skipWaiting();
 });
 
 // Activate event
@@ -23,34 +11,19 @@ self.addEventListener('activate', event => {
     caches.keys().then(cacheNames => {
       return Promise.all(
         cacheNames.map(cacheName => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
+          return caches.delete(cacheName);
         })
       );
     })
   );
+  self.clients.claim();
 });
 
-// Fetch event
+// Fetch event - no caching
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      if (response) {
-        return response;
-      }
-      return fetch(event.request).then(response => {
-        if (!response || response.status !== 200 || response.type === 'error') {
-          return response;
-        }
-        const responseToCache = response.clone();
-        caches.open(CACHE_NAME).then(cache => {
-          cache.put(event.request, responseToCache);
-        });
-        return response;
-      }).catch(() => {
-        return caches.match('/offline.html');
-      });
+    fetch(event.request).catch(() => {
+      return caches.match('/offline.html');
     })
   );
 });
